5aeb8843494050d0dc093623bbfa1f4eda90dda0
aniruddhakhan <aniruddhakhan747@gmail.com>
2024-03-29 21:58:15 -0600

    Finished the produc page . Amin and Ani

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -2,6 +2,9 @@
 
 import { Product } from "@/types";
 import Image from "next/image";
+import IconButton from "@/components/ui/icon-button";
+import {  Expand, ShoppingCart } from "lucide-react";
+import Currency from "@/components/ui/currency";
 interface ProductCard{
     data: Product;
 } 
@@ -17,9 +20,33 @@ const ProductCard: React.FC<ProductCard> = ({
                     src={data?.images?.[0]?.url}
                     fill
                     alt ="Image"
-
-
+                    className="aspect-square object-cover rounded-md"
                 />
+                <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+                    <div className="flex gap-x-6 justify-center">
+                        <IconButton
+                            onClick={() => {}}
+                            icon={<Expand size={20} className="text-gray-600 "/>}
+                        />
+                        <IconButton
+                            onClick={() => {}}
+                            icon={<ShoppingCart size={20} className="text-gray-600 "/>}
+                        />
+                    </div>
+                </div>
+            </div>
+            {/*Description*/}
+            <div>
+                <p className="font-bold text-lg text-center">
+                    {data.name}
+                </p>
+                <p className="text-sm text-black text-center">
+                    {data.category?.name}
+                </p>
+            </div>
+            {/*Price*/}
+            <div className="items-center justify-between text-center">
+                <Currency value={data?.price}/>
             </div>
         </div>
      );
