a2aaa9e5911ffad4d1bd0ceb0992bd10b2ab5d2c
aniruddhakhan <aniruddhakhan747@gmail.com>
2024-03-29 20:35:56 -0600

    Added products type and added the mechanism to fetch products

--- types.ts
+++ types.ts
@@ -1,3 +1,5 @@
+import { SizeLimit } from "next";
+
 export interface Billboard {
     id: string;
     label: string;
@@ -10,3 +12,29 @@ export interface Category {
  billboard: Billboard;
 };
 
+export interface Product {
+    id: string;
+    category: Category;
+    name: string;
+    price: string;
+    isFeatured: boolean;
+    size: Size;
+    color: Color;
+    images: Image[];
+};
+
+export interface Image{
+    id: string;
+    url: string;
+};
+
+export interface Size{
+    id: string;
+    name: string;
+    value: string; 
+};
+export interface Color{
+    id: string;
+    name: string;
+    value: string; 
+};
