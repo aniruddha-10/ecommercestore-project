152b25d0724b3fcf1af13f2653f30050473bce71
aniruddhakhan <aniruddhakhan747@gmail.com>
2024-04-10 03:39:22 -0600

    added the add to cart functionality partially (Amin)

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Urbanist } from "next/font/google";
 import Footer from '@/components/footer'
 import Navbar from "@/components/navbar"
 import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 const font = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
@@ -21,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ModalProvider/>
+        <ToastProvider/>
         <Navbar /> 
         {children}
         <Footer /> 
