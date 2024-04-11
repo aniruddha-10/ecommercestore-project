95c41391096288ed406e8efd632741a57f87c4ce
aniruddhakhan <aniruddhakhan747@gmail.com>
2024-04-11 02:06:22 -0600

    Changed a minor spelling mistake

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
        <footer className = "bg-black border-t">
         <div className ="mx-auto py-10">
         <p className = "text-center text-xs text-white">
-            &copy; 2024 ECommerceStore, Incorporation, All rights reserved for us motherfuckers.
+            &copy; 2024 ECommerceStore, Incorporation, All rights reserved for us.
         </p>
         </div>
        </footer> 
